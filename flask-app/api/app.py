06d60acdf38fc516ac44657c3c8b9a3e5a98cdb3
Jing Hong Teh <jhteh2000@gmail.com>
2024-01-31 16:02:35 +0000

    Filtering data on api server side

--- flask-app/api/app.py
+++ flask-app/api/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from books import books
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
+    query = request.args.get("query")
+    filter = request.args.get("filter")
+
+    if query and filter:
+        filtered_data = [i for i in books if str.lower(query) in str.lower(str(i[filter]))]
+        return jsonify(filtered_data)
+
     return jsonify(books)
